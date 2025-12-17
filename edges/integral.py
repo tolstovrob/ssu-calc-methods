d4d16678d12c5023007e19e274c13e564387002b
tolstovrob <tolstovrob@gmail.com>
2025-12-18 01:17:00 +0400

    чё-то добавил

---
+++ edges/integral.py
@@ -0,0 +1,11 @@
+"""
+Модуль edge_implementation.py реализует решение краевой задачи
+с использованием базового класса BaseEdgeSolver
+"""
+
+from typing import Callable, Optional, Tuple, List
+import math
+
+from base_solver import BaseSolver, SolutionResult
+from base_edge_solver import BaseEdgeSolver
+
